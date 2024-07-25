7228edd8cb6c8e002add2427bbc437e0488b0af5
Shivam Srivastava <shivamsrivastava.9@gmail.com>
2024-07-25 23:36:24 +0530

    file write

--- file.py
+++ file.py
@@ -1,13 +1,20 @@
 
-file=open("temp.txt")
+file=open("./temp.txt")
 print(file)
-
 contents=file.read()
 print(contents)
 file.close() # manually close
+
+file2=open("temp.txt",mode="a")
+for i in range(10):
+    file2.write(f"{i}. YO\n")
+#file2.append("\n======")
 
 
 with open("config.json") as fileJson: # auto close after read
     contentsJson=fileJson.read()
     print(contentsJson)
+    print(type(contentsJson))
+    
+    
     
