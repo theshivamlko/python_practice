93545b6cc54a21898ab8360987be986d042b4f70
Shivam Srivastava <shivamsrivastava.9@gmail.com>
2024-07-25 17:56:50 +0530

    save

--- oops.py
+++ oops.py
@@ -14,11 +14,12 @@ class Fish(Animal):
         super().__init__()
         self.fins=True
         self.name="Nemo"
+        self.numEyes=4
         print("Fish init")
         
 
     def swim(self):
-        print(f"Fish {self.name} Moving in water {self.fins}")
+        print(f"Fish {self.name} Moving in water {self.fins} {self.numEyes}")
      
     def breath(self):
         super().breath()
