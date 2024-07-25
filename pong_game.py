97c0076224d42d9c73fe5e175dccd6b7ac505591
Shivam Srivastava <shivamsrivastava.9@gmail.com>
2024-07-25 20:42:43 +0530

    paddle

--- pong_game.py
+++ pong_game.py
@@ -1,9 +1,30 @@
-from turtle import Screen
-import sys
-print(sys.path)
+from turtle import Screen , Turtle
+# import sys
+# print(sys.path)
 
 screen=Screen()
 screen.bgcolor("black")
 screen.setup(width=800,height=600)
+
+
+paddle =Turtle()
+paddle.shape("square")
+paddle.color("white")
+paddle.shapesize(stretch_wid=5,stretch_len=1)
+paddle.penup()
+paddle.goto(350,0)
+
+
+def go_up():
+    newY=paddle.ycor()+20
+    paddle.goto(paddle.xcor(),newY)
+
+def go_down():
+    newY=paddle.ycor()-20
+    paddle.goto(paddle.xcor(),newY)
+
+screen.listen()
+screen.onkey(go_up,"Up")
+screen.onkey(go_down,"Down")
 
 screen.exitonclick()
